30197d8316d1607ec57684a1b954edcb7d5928f2
navkrishna <krishna2510@gmail.com>
2015-10-16 02:04:54 +0530

    created BuzzzzDetailActivity

---
+++ app/src/main/java/in/buzzzz/activity/BuzzzzDetailActivity.java
@@ -0,0 +1,17 @@
+package in.buzzzz.activity;
+
+import android.os.Bundle;
+
+import in.buzzzz.R;
+
+/**
+ * Created by Navkrishna on October 16, 2015
+ */
+public class BuzzzzDetailActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buzzzz_detail);
+    }
+}
